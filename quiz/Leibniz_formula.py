93187dbe6d12d11d4780b1ac91b87a7227155474
Yuji Yamamoto <y.yama683@gmail.com>
2020-02-07 14:41:42 +0900

    add Leibniz

---
+++ quiz/Leibniz_formula.py
@@ -0,0 +1,57 @@
+
+def Leibniz_series(n):
+    """
+    n項目のライプニッツ級数を求める。
+    ただし、初項は0項目と数え、引数が負の場合は値を返さない。
+
+    Parameters
+    -------
+    n : int
+        項数。
+
+    Returns
+    -------
+    float or None
+        n項目のライプニッツ級数
+
+    Examples
+    --------
+    >>> print(Leibniz_series(0))
+    1.0
+    >>> print(Leibniz_series(2))
+    0.2
+    >>> print(Leibniz_series(10))
+    0.047619047619047616
+    >>> print(Leibniz_series(-1))
+    None
+    """
+    # TODO
+    pass
+
+
+def find_pi(func, n, coef):
+    """
+    初項からn項までの和に適切な係数をかけて円周率の近似値を求める。
+
+    Parameters
+    -------
+    func : function
+        n番目の級数を返す関数
+    n : int
+        項数
+    coef : float
+        係数
+
+    Returns
+    -------
+    float
+        円周率の近似値
+    """
+    # TODO
+    pass
+
+
+if __name__ == "__main__":
+    n = 1000
+    print("円周率の近似値:")
+    print(find_pi(Leibniz_series, n, coef=4))
