6355aea97a6bf565dcf76f22a17c195463de28dc
Yuto-tus <55906260+Yuto-tus@users.noreply.github.com>
2020-04-09 19:28:27 +0900

    Update quiz05.py

--- quiz/quiz05.py
+++ quiz/quiz05.py
@@ -1,13 +1,16 @@
-    # Hello, World!
-import math, statistics
+# Hello, World!
 
 
-def hoge( a, b= 1):
-    tmp = 1
+def hoge(a, b=1):
     return sum(a)
 
+
 if __name__ == "__main__":
-    huga={'key1' : 1 , 'key2': 2} #頑張れ
-    hoge(huga.values( ))
-    O = 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20 + 21 + 22 + 23 + 24 + 25 + 26 + 27 + 28 + 29
-    print(O)
+    huga = {'key1': 1, 'key2': 2}  # 頑張れ
+    hoge(huga.values())
+    order = 0 + 1 + 2 + 3 + 4 + 5 +\
+        6 + 7 + 8 + 9 + 10 + 11\
+        + 12 + 13 + 14 + 15 + 16\
+        + 17 + 18 + 19 + 20 + 21 +\
+        22 + 23 + 24 + 25 + 26 + 27 + 28 + 29
+    print(order)
