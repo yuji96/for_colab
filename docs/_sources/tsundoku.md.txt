0c6ea7da4ec3e7ea98ceee26a868cab1e62feb97
Yuji Yamamoto <y.yama683@gmail.com>
2021-02-18 17:23:54 +0900

    add all tsundoku

---
+++ docs/_sources/tsundoku.md.txt
@@ -0,0 +1,42 @@
+# Tsundoku
+
+  &emsp;「後で読も」と言って全く読んでいないものを多く含む。
+  名前通り、積読なので整理はされてない。
+  おすすめ度が高いものはfilterに載せる予定。
+
+
+## プログやリポジトリなど
+
+  &emsp;Wiki形式のサイトは検索ランキングが高くなりがちだが、個人ブログや個人リポジトリの方が解説・サンプルコードが充実してたりする。
+
+  - [機械学習に詳しくなりたいブログ](https://www.iwanttobeacat.com/)
+  - [数学に強いエンジニアむけの機械学習勉強法](https://medium.com/@hayato.iida.0213/%E6%95%B0%E5%AD%A6%E3%81%AB%E5%BC%B7%E3%81%84%E3%82%A8%E3%83%B3%E3%82%B8%E3%83%8B%E3%82%A2%E3%82%80%E3%81%91%E3%81%AE%E6%A9%9F%E6%A2%B0%E5%AD%A6%E7%BF%92%E5%8B%89%E5%BC%B7%E6%B3%95-e3f4bd7a7cf9)
+  - [Kaggleできっと役立つTipsまとめ](https://naotaka1128.hatenadiary.jp/entry/kaggle-compe-tips)
+    - [ML_BearのKaggleな日常](https://naotaka1128.hatenadiary.jp/)
+  - [Colabで試せる面白い機械学習プロジェクト6選](https://tkrel.com/10041)  
+    &emsp;「動画中の車認識、画像内のテキスト抽出、GANによる2つのイメージを合成、音声をボーカルと音楽に分離、強化学習、Altairによる可視化」などが
+    Google Colaboratoryで簡単に試せる。Alteirって初めて聞いたけど、主流のseabornみたいな感じなので深掘りする必要はなさそう。
+    - [Altair docs](https://altair-viz.github.io/gallery/index.html)
+  - [DeOldify](https://github.com/jantic/DeOldify)  
+    &emsp;白黒の動画像をカラー化するアルゴリズム。
+  - [データサイエンス100本ノック](https://github.com/The-Japan-DataScientist-Society/100knocks-preprocess)  
+    &emsp;Dockerがcsvファイルを元に生成したdbを`pandas.read_sql`で読み込んで実行することが本来の問題だが、
+    csvファイルを直接`pandas.read_csv`で読み込めばDocker, SQLの知識は必要なくPandasのみで実行できる。
+  - [PRMLの再現プログラム](https://github.com/ctgk/PRML)
+  - [暗黒通信弾 PRMLの補足](https://herumi.github.io/prml/)
+  - [数理・情報教育研究センター（東大） e-learning教材](http://www.mi.u-tokyo.ac.jp/e-learning.html)
+  - [Dash](https://dash-gallery.plotly.host/Portal/)  
+    &emsp;超多機能可視化ツール。データ分析用というよりwebページ作成用？
+  - [Pandasのplotの全引数を解説](https://own-search-and-study.xyz/2016/08/03/pandas%E3%81%AEplot%E3%81%AE%E5%85%A8%E5%BC%95%E6%95%B0%E3%82%92%E4%BD%BF%E3%81%84%E3%81%93%E3%81%AA%E3%81%99/)
+
+
+## メディア
+
+  - [@IT](https://www.atmarkit.co.jp/ait/series/search/17/)
+    &emsp;中途半端なところから会員限定になる記事も多い。
+    シンタックスハイライトが他サイトに比べると格好良くなかったり、教科書や公式ドキュメントレベルほど充実した内容ではないため、
+    流し見しながら初見の用語があったら他サイトで深掘りする感じで使っている。
+    中には面白かったり役に立ったりする記事もある。
+  - [わかばちゃんと学ぶGit](https://www.r-staffing.co.jp/engineer/entry/20190621_1)  
+    私はgitはこれで習得したと言っても過言では無い（ちょっと盛ってる）。
+    [機械学習編](https://www.r-staffing.co.jp/engineer/entry/20210122_1)も連載予定らしい。
--- docs/_static/createItems.js
+++ docs/_static/createItems.js
@@ -1,30 +1,19 @@
-let data = [
-  // ["なまえ", "いろ", "れべる", "たぐ", "りんく"],
-  ["松尾研GCI", "#0fbcf9", "", "wip 無料", "https://weblab.t.u-tokyo.ac.jp/gci_contents/"],
-  ["松尾研DL4US", "#0fbcf9", "", "wip 無料", "https://weblab.t.u-tokyo.ac.jp/dl4us/"],
-  ["PRML", "#ffd32a", "", "理論 書籍 英語版無料", "https://www.microsoft.com/en-us/research/people/cmbishop/prml-book/"],
-  ["はじぱた", "#0be881", "", "理論 書籍", "https://www.morikita.co.jp/books/book/2235"],
-  ["大学図書館", "", "", "理論 wip 書籍 無料(∵学費)", ""],
-  ["丸善 eBook", "", "", "理論 wip 書籍 無料(∵学費)", "https://elib.maruzen.co.jp/elib/html/GuestLogin?1"],
-  ["Deep Learning (MIT Press)", "", "", "理論 書籍 英語版無料", "https://www.deeplearningbook.org/"],
-  ["Dash", "", "", "ライブラリ 可視化", "https://dash-gallery.plotly.host/Portal/"],
-  ["AVILEN", "", "", "メディア 無料", "https://ai-trend.jp/"],
-  ["データサイエンスハンドブック", "", "", "Python numpy pandas matplotlib scikit-learn 英語版無料", "https://jakevdp.github.io/PythonDataScienceHandbook/"],
-  ["コーディング規約", "", "", "Python", "https://pep8-ja.readthedocs.io/ja/latest/"],
-  ["AIZU", "", "", "Python 無料", "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1"],
-  ["東大公開授業資料", "", "", "Python numpy pandas matplotlib scikit-learn 無料", "https://utokyo-ipp.github.io/index.html"],
-  ["日本医療AI学会", "", "", "wip 無料", "https://japan-medical-ai.github.io/medical-ai-course-materials/index.html"],
-  ["NewYork Univ. DL (日本語)", "", "", "wip 深層学習", "https://atcold.github.io/pytorch-Deep-Learning/ja/"],
-  ["Stanford Univ. CNN", "", "", "理論 動画", "https://www.youtube.com/playlist?list=PL3FW7Lu3i5JvHM8ljYj-zLfQRF3EO8sYv"],
-  ["3B1B NN", "", "", "理論 動画", "https://www.youtube.com/playlist?list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi"],
-  ["3B1B 線形代数", "", "", "理論 動画", "https://www.youtube.com/playlist?list=PLZHQObOWTQDPD3MizzM2xVFitgF8hE_ab"],
-  ["3B1B 微積", "", "", "理論 動画", "https://www.youtube.com/playlist?list=PLZHQObOWTQDMsr9K-rj53DwVRMYO3t5Yr"],
-  // ["", "", "", "", ""],
-],
-  allTags = ["理論", "書籍", "ライブラリ", "メディア", "無料", "Python", "numpy", "pandas", "matplotlib", "scikit-learn", "深層学習", "動画"],
+let allTags = [
+    "理論",
+    "書籍",
+    "ライブラリ",
+    "メディア",
+    "無料",
+    "Python",
+    "numpy",
+    "pandas",
+    "matplotlib",
+    "scikit-learn",
+    "深層学習",
+    "動画",
+  ],
   gridElem = $(".grid"),
   filterControl = $(".filter-field");
-
 
 for (let i = 0; i < data.length; i++) {
   let title = data[i][0],
@@ -40,7 +29,9 @@ for (let i = 0; i < data.length; i++) {
   item.attr({ "data-title": title, "data-tag": tags });
   item.find(".custom-content").css("background", color);
   item.find(".item-title").text(title);
-  item.find("a").attr({"href": link, "target": "_blank", "rel": "noopener noreferrer"});
+  item
+    .find("a")
+    .attr({ href: link, target: "_blank", rel: "noopener noreferrer" });
 
   tagsElem = item.find(".item-tags");
   for (let i = 0; i < tags.length; i++) {
